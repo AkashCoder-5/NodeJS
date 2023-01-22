cd7b21b29a99d3c46c9265107538dd4725068660
pkv19970 <pkv19970@gmail.com>
2023-01-22 13:23:53 +0530

    This is the first commit

--- App.js
+++ App.js
@@ -1,1 +1,19 @@
-This is demo git file
+const { ok } = require('assert');
+const http = require('http');
+
+http.createServer((req, res) => {
+    if (req.url === '/hello') {
+        res.write('This is hello world');
+        res.end();
+    }
+
+    if (req.url === '/json') {
+        result = { firstName: 'pawan', lastName: 'kumar', middleName: 'vishwakarma' };
+        res.writeHead(200)
+        res.write(JSON.stringify(result));
+        res.end();
+    }
+
+    res.statusCode = 404;
+    res.end();
+}).listen(3000);
