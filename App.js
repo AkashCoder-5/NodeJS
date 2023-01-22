f57d012fb44ced62e3441bc6d44d6c94a98b5d0f
pkv19970 <pkv19970@gmail.com>
2023-01-22 13:46:01 +0530

    Initial commit

--- App.js
+++ App.js
@@ -1,1 +1,8 @@
-This is demo git file
+var Express = require('express');
+var express = new Express();
+express.get('/', (req, res) => {
+    res.send('hello express');
+    res.end();
+});
+
+express.listen(2000);
